195f237a952412c03601cb8697a42a4e80f52f1c
Niharika <97402437+niharika1102@users.noreply.github.com>
2023-06-17 13:06:17 +0530

    Create Chef Plays Ludo.java

---
+++ Chef Plays Ludo.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int testCase = input.nextInt();
+		
+		for (int i = 0; i < testCase; i++) {
+		    int number = input.nextInt();
+		    
+		    if (number == 6) {
+		        System.out.println("YES");
+		    }
+		    else {
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
