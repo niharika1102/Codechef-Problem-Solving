4d12cd90f000bfe1d33ad1aa41a3ac04e7e31449
Niharika <97402437+niharika1102@users.noreply.github.com>
2023-06-20 16:38:40 +0530

    Create Tom and Jerry Chase.java

---
+++ Tom and Jerry Chase.java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int testCase = input.nextInt();
+		
+		for (int i = 0; i < testCase; i++) {
+		    int jerry = input.nextInt();
+		    int tom = input.nextInt();
+		    
+		    if (tom > jerry) {
+		        System.out.println("YES");
+		    }
+		    else {
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
