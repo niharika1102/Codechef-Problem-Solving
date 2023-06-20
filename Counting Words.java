ccfbe411be418b9e71c96b725a9a8c3ec8cc8337
Niharika <97402437+niharika1102@users.noreply.github.com>
2023-06-20 16:28:20 +0530

    Create Counting Words.java

---
+++ Counting Words.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int testCase = input.nextInt();
+		
+		for (int i = 0; i < testCase; i++) {
+		    int pages = input.nextInt();
+		    int words = input.nextInt();
+		    
+		    int total = pages * words;
+		    System.out.println(total);
+		}
+	}
+}
