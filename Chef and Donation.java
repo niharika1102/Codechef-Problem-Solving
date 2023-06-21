ea6eb3790b456c4667cfaa40fd9584d6eb7cbed9
Niharika <97402437+niharika1102@users.noreply.github.com>
2023-06-21 16:35:02 +0530

    Create Chef and Donation.java

---
+++ Chef and Donation.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int testCase = input.nextInt();
+		
+		for (int i = 0; i < testCase; i++) {
+		    int chef = input.nextInt();
+		    int chefina = input.nextInt();
+		    
+		    int charity = chefina - chef;
+		    
+		    System.out.println(charity);
+		}
+	}
+}
