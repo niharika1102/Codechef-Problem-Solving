61f9fc1c85abe6a86d2378f17f746587712d7ed4
Niharika <97402437+niharika1102@users.noreply.github.com>
2023-06-22 11:21:09 +0530

    Create Mana Points.java

---
+++ Mana Points.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int testCase = input.nextInt();
+		
+		for (int i = 0; i < testCase; i++) {
+		    int cost = input.nextInt();
+		    int points = input.nextInt();
+		    
+		    int attacks = points / cost;
+		    System.out.println(attacks);
+		}
+	}
+}
